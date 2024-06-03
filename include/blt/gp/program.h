a18c114cecc28c628afcca3e6489b53cf1b24a3a
Brett Laptop <tri11paragon@tpgc.me>
2024-06-02 21:27:00 -0400

    running into type system issues again

---
+++ include/blt/gp/program.h
@@ -0,0 +1,67 @@
+#pragma once
+/*
+ *  Copyright (C) 2024  Brett Terpstra
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef BLT_GP_PROGRAM_H
+#define BLT_GP_PROGRAM_H
+
+#include <blt/gp/fwdecl.h>
+#include <functional>
+#include <blt/std/ranges.h>
+#include <type_traits>
+
+namespace blt::gp
+{
+    class identifier
+    {
+    };
+    
+    template<typename Return, typename Args>
+    class operation
+    {
+        public:
+            using function_t = std::function<Return(blt::span<Args>)>;
+            
+            operation(const operation& copy) = default;
+            
+            operation(operation&& move) = default;
+            
+            template<typename T>
+            explicit operation(const T& functor)
+            {
+                if constexpr (std::is_same_v<T, function_t>)
+                {
+                    func = functor;
+                } else
+                {
+                    func = [&functor](blt::span<Args> args) {
+                        return functor(args);
+                    };
+                }
+            }
+            
+            explicit operation(function_t&& functor): func(std::move(functor))
+            {}
+        
+        private:
+            function_t func;
+    };
+    
+    
+}
+
+#endif //BLT_GP_PROGRAM_H
